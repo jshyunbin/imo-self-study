e28abe837b7caf6d4c7df48433d085773fe52a10
Joshua Hyunbin Lee <jshyunbin@gmail.com>
2026-09-17 15:55:21 +0900

    3d initial setup and peggy bvh parser initialized

--- 1-2_BVH_player/main.js
+++ 1-2_BVH_player/main.js
@@ -1,23 +1,54 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(0x808080);
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const camera = new THREE.PerspectiveCamera(30, 1, 0.1, 1000);
+camera.position.set(-28, 28, 10);
+camera.lookAt(scene.position);
 
-camera.position.z = 5;
+var controls = new OrbitControls(camera, renderer.domElement);
+controls.damping = 0.2;
+
+// ADAPT TO WINDOW RESIZE
+function resize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+}
+window.addEventListener('resize', resize);
+resize();
+
+const input = document.querySelector("input");
+input.addEventListener("change", updateBVHFile);
+
+
+
+
+
+
+
+
+function updateBVHFile() {
+
+}
+
+
+const axesHelper = new THREE.AxesHelper( 3 );
+scene.add( axesHelper );
+
+const gridHelper = new THREE.GridHelper( 10, 10 );
+scene.add( gridHelper );
+
+
 
 function animate( time ) {
-  cube.rotation.x = time / 2000;
-  cube.rotation.y = time / 1000;
-  
+
   renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
